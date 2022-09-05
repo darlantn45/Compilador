46c0a608f1b394890f2d1ca2f1dba30cb03ded0e
Darlan Oliveira Santos <55238348+darlantn45@users.noreply.github.com>
2022-09-05 16:13:09 -0300

    Add files via upload

--- ProximoCaracter.java
+++ ProximoCaracter.java
@@ -13,20 +13,28 @@ public class ProximoCaracter {
 	public char proximo() {
 		if(linha != null) {
 		if( aux == linha.length()) {
-		linha = ldat.lerLinha();
-		aux = 0;
-		}
-		if(linha != null) {
-			char c = linha.charAt(aux);
-			aux++;
-			if(c == '#') aux = linha.length();
-			//System.out.print(c);
-			return c;
+			//System.out.println(linha);
+			
+			do {
+
+				linha = ldat.lerLinha();
+				if(linha == null) {
+				return'\0';
+				}
+	//			System.out.println(linha);
+				aux = 0;
+				}while(linha.length() <= 0);
+			}
+		
+		char c = linha.charAt(aux);
+		aux++;
+		if(c == '#') {aux = linha.length(); return proximo();
+}
+		return c;
+		//System.out.print(linha);
 		}else {
 			return '\0';
 		}
-		}
-		return '\0';
 	}
 	public void decrementa() {
 		aux--;
